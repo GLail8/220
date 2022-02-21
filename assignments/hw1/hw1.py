2fa50170f08b5515f4c5416d9a944cea5d1ad474
GLail8 <garrett597139@gmail.com>
2022-02-21 16:43:48 -0500

    Completed Homework 1

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -1,35 +1,48 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
-
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
-
-Certification of Authenticity:
-<include one of the following>
+Name: Garrett Lail
+hw1.py
+Solved problems for Homework 1
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
-
+#this program allows users to calculate the area of a rectangle
 def calc_rec_area():
-    pass
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    area = length * width
+    print("Area =", area)
 
 
+#this program allows the user to calculate the volume of a rectangular solid
 def calc_volume():
-    pass
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    height = eval(input("Enter the height:"))
+    volume = length * width * height
+    print("volume=", volume)
 
 
+#this program calculates a players shooting percentage
 def shooting_percentage():
-    pass
+    total = eval(input("Enter the player's total shots: "))
+    made = eval(input("Enter how many shots the player made: "))
+    shooting = (made / total) * 100
+    print("Shooting Percentage: ", shooting, "%")
 
 
+#this calculates the total cost of coffee per pound
 def coffee():
-    pass
+    order = eval(input("How many pounds of coffee would you like? "))
+    cost = 10.50
+    shipping = 0.86
+    fixed = 1.50
+    total = (cost * order) + (shipping * order) + fixed
+    print("Your total is: ", total)
 
 
+#this program converts kilometers to miles
 def kilometers_to_miles():
-    pass
-
-
-if __name__ == '__main__':
-    pass
+    km = eval(input("How many kilometers did you travel?"))
+    mile = 1.61
+    convert = km / mile
+    print("That's", convert, "miles!")
