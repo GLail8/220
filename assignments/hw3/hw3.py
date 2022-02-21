ea15c5ac4180232776d0e79f29d8b787e4c5bd7b
GLail8 <garrett597139@gmail.com>
2022-02-21 16:47:02 -0500

    Completed Homework 3

--- assignments/hw3/hw3.py
+++ assignments/hw3/hw3.py
@@ -1,35 +1,57 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Garrett Lail
+hw3.py
 
 Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
 
-Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
-
+#finds the average of the grades input
 def average():
-    pass
+    grade_total = eval(input("how many grades will you enter? "))
+    grade_avg = 0
+    for i in range(grade_total):
+        grade = eval(input("Enter grade: "))
+        grade_avg = grade_avg + grade
+    average = grade_avg / grade_total
+    print("average is", average)
 
-
+#calculates the tips from 5 people
 def tip_jar():
-    pass
+    tip_total = 0
+    for j in range(5):
+        tip = eval(input("how much would you like to donate?"))
+        tip_total = tip_total + tip
+    print("total tips: ", tip_total)
 
-
+#calculates the approximate root using Newton's method
 def newton():
-    pass
-
+    x = eval(input("What number do you want to square root? "))
+    impr = eval(input("How many times should we improve the approximation? "))
+    approx = x
+    for k in range(impr):
+        approx = (approx + (x / approx)) / 2
+    print("the square root is approximately", approx)
 
 def sequence():
-    pass
+    sequence = eval(input("how many terms would you like?"))
+    for l in range(sequence):
+        print(2*l+1, end=" ")
 
 
+#computes the approximate value of pi using the wallis formula
 def pi():
-    pass
-
+    series = eval(input("how may terms in the series?"))
+    wallis = 1
+    num = 2
+    for m in range(series):
+        term1 = num / (num -1)
+        term2 = num / (num+1)
+        wallis = wallis * term1 * term2
+        num = num + 2
+    pi = wallis * 2
+    print(pi)
 
 if __name__ == '__main__':
     pass
